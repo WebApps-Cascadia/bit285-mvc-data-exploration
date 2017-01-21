913fbec57ddf713b0cf8c557d7a7f0ce28423e9c
ZINMAUNGA574\zinmaung <zmm27@yahoo.com>
2017-01-20 16:10:24 -0800

    First Part Done

--- MVC-DataExploration/Controllers/CustomerController.cs
+++ MVC-DataExploration/Controllers/CustomerController.cs
@@ -16,5 +16,24 @@ namespace MVC_DataExploration.Controllers
         {
             return View(db.Customers);
         }
+
+        //Get Create - send the view with the little  boxes
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(); //Show the form (the little boxes)
+        }
+
+        //POST Create - Get the data, Work with the model to update the database with data from the form.
+        [HttpPost]
+        public ActionResult Create(Customer myCustomer)
+        {
+            //Add the new coustomer to my set Customers
+            db.Customers.Add(myCustomer);
+
+            //Update the database
+            db.SaveChanges();   
+            return View("Index", db.Customers); // Show the updaded Index page
+        }
     }
 }

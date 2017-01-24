4a54baedbb7f2980367637e44630759dac452423
JonL_ <JonL_@JON-XPS>
2017-01-23 20:56:55 -0800

    Assignment Complete.

--- MVC-DataExploration/Controllers/ProductController.cs
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//using System.Collections.ObjectModel.
+using MVC_DataExploration.Models;
 
 namespace MVC_DataExploration.Controllers
 {
     public class ProductController : Controller
     {
-        //private MyDBContext db = new MyDBContext();
+        private MyDBContext db = new MyDBContext();
         
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            return View(db.Products);
         }
     }
 }

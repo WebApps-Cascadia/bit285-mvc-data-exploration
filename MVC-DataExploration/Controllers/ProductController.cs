f97e8721c5471ce84ef83f1e18a06eeddf709c49
JonL_ <JonL_@JON-XPS>
2017-01-23 20:46:11 -0800

    Working on Part 2.12

---
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using System.Collections.ObjectModel.
+
+namespace MVC_DataExploration.Controllers
+{
+    public class ProductController : Controller
+    {
+        //private MyDBContext db = new MyDBContext();
+        
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

11f6ed8dddf64fa4dc461fcd8a73f4c3c5a81648
Joanie Dawson <dawson.joan.m@gmail.com>
2018-01-23 14:11:45 -0800

    code first changes

--- MVC-DataExploration/Controllers/CustomerController.cs
+++ MVC-DataExploration/Controllers/CustomerController.cs
@@ -11,6 +11,19 @@ namespace MVC_DataExploration.Controllers
     {
         private MyDbContext db = new MyDbContext();
 
+        //POST: Customer
+        [HttpPost]
+        public ActionResult Create()
+        {
+           // Pass the Create() method a Customer object, called customer
+            Customer customer = new Customer();
+            //Use the db.Customers Add() method to add the passed Customer to the model
+            db.Customers.Add(customer);
+            //Use the db.SaveChanges() method to update the Database (the first time run, this will create the database too)
+            db.SaveChanges();
+            //Send the user back an updated Index page by returning an Index view with the db.Customers model, return View("Index",db.Customers)
+            return View("Index", db.Customers);
+        }
         // GET: Customer
         public ActionResult Index()
         {

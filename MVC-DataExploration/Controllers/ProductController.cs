5ecc23374f3b7a28a9b62cb26067853c4de7db2b
Sudhana Lai <Mr.dudeguymann@gmail.com>
2018-01-23 15:34:30 -0800

    Step 12 Finished

--- MVC-DataExploration/Controllers/ProductController.cs
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_DataExploration.Models;
 
 namespace MVC_DataExploration.Controllers
 {
     public class ProductController : Controller
     {
+        private MyDBContext db = new MyDBContext();
         // GET: Product
         public ActionResult Index()
         {
-            return View();
+            return View(db.Products);
         }
     }
 }

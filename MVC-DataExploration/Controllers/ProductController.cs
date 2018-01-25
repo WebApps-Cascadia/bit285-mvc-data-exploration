4e108e70376cfbf77c35dd2fe414e438424b8e38
BenC <ben@ceterisdev.com>
2018-01-24 23:51:37 -0800

    Made updates to database-first Could not get Product Index view to work without null exception

---
+++ MVC-DataExploration/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_DataExploration.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index(Models.Product mp)
+        {
+            return View();
+        }
+    }
+}

a933a70558f23224f2920016406f7ba22d1c0e27
Joanie Dawson <dawson.joan.m@gmail.com>
2018-02-06 15:29:09 -0800

    Code first Revision

--- MVC-DataExploration/Controllers/CustomerController.cs
+++ MVC-DataExploration/Controllers/CustomerController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC_DataExploration.Models;
-
+//revision
 namespace MVC_DataExploration.Controllers
 {
     public class CustomerController : Controller
--- MVC-DataExploration/Models/Customer.cs
+++ MVC-DataExploration/Models/Customer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-
+//Revision
 namespace MVC_DataExploration.Models
 {
     public class Customer
